dd900baafb9ee7abeed8eef6802653a895c80458
jmgcito <joshuamguillen@gmail.com>
2021-10-18 11:06:14 -0700

    Soundboard V1.00

--- src/index.js
+++ src/index.js
@@ -5,24 +5,30 @@ import "./styles.css";
 import reportWebVitals from "./reportWebVitals";
 import squish from "./audio/squish.mp3";
 import wow from "./audio/wow.mp3";
+import youre20 from "./audio/youre20.mp3";
+import notthatguy from "./audio/yourenotthatguy.mp3";
+import heyyeayea from "./audio/heyyeayea.mp3";
+import wompwompwomp from "./audio/wompwompwomp.mp3";
 import { Howl, Howler } from "howler";
 
-const Scale = (props) => {
-  return (
-    <button className="button" type="button" onClick={props.onClick}>
-      {props.name}
-    </button>
-  );
-};
+const soundClips = [
+  { sound: squish, label: "squish" },
+  { sound: wow, label: "w000w" },
+  { sound: youre20, label: "you're 20?" },
+  { sound: heyyeayea, label: "hey yay yeaaaah" },
+  { sound: wompwompwomp, label: "womp womp wooomp" },
+  { sound: notthatguy, label: "you're not that guy" },
+];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "Click a button",
+      message: "click a button",
       song: process.env.PUBLIC_URL + "thedeli.mp3",
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.buttonAndSound = this.buttonAndSound.bind(this);
+    this.soundPlay = this.soundPlay.bind(this);
   }
   soundPlay = (src) => {
     const sound = new Howl({
@@ -30,34 +36,34 @@ class App extends React.Component {
     });
     sound.play();
   };
-
-  handleClick() {
-    this.setState({
-      message: "playing...",
+  buttonAndSound = () => {
+    return soundClips.map((soundObj, index) => {
+      return (
+        <button
+          className="button"
+          type="button"
+          onClick={() => {
+            this.soundPlay(soundObj.sound);
+          }}
+        >
+          {soundObj.label}
+        </button>
+      );
     });
-  }
+  };
+
   render() {
-    const { name } = this.props;
     Howler.volume(1.0);
 
     return (
-      <div>
-        <h1 className="vertical-center">Mwahahahahahaaaaaaaaa</h1>
-        <div className="vertical-center">
-          <Scale
-            name="Squish"
-            onClick={() => {
-              this.soundPlay(squish);
-            }}
-          />
-          <Scale
-            name="W0W"
-            onClick={() => {
-              this.soundPlay(wow);
-            }}
-          />
-        </div>
-        <h1 className="vertical-center">{this.state.message}</h1>
+      <div className="vertical-center">
+        <article className="card middle">
+          <div>
+            <h1 className="text">Jmgcito's Favorite Sounds</h1>
+          </div>
+          <div>{this.buttonAndSound()}</div>
+          <h2 className="text">{this.state.message}</h2>
+        </article>
       </div>
     );
   }
